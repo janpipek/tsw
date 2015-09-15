de3b00050d8b749a13311b62e9b5481cfe4a1d90
Jan Pipek <jan.pipek@gmail.com>
2015-09-15 14:31:49 +0200

    Caching + column names

--- test.cc
+++ test.cc
@@ -4,10 +4,15 @@ using namespace tsw;
 
 int main()
 {
-    ThreadSafeWriter<int, int, std::string> writer{{"i", "j", "k"}};
+    TSVWriter<int, int, std::string> writer;//{{"i", "j", "k"}};
+    writer.SetColumnNames("aa", "bb", "cc");
     std::string f = "ahoj";
     std::tuple<int, int, std::string> x{1, 2, f};
-    writer.Store(1, 2, "ahoj");
-    writer.Flush();
+    writer.SetCacheCapacity(10);
+    for (int i = 1; i < 100; i++)
+    {
+        writer.Store(i, i * i, "ahoj");
+    }
+    // writer.Flush();
     // writer.Write({4, 4, std::string("ahoj")});
 }
--- tsw.hh
+++ tsw.hh
@@ -8,52 +8,117 @@
 
 namespace tsw
 {
-    template <class... Ts> class ThreadSafeWriter
-    {  };
+    template <class... Ts> class ThreadSafeWriter {  };
 
     template <class U, class... Ts> class ThreadSafeWriter<U, Ts...>
     {
-        public:
-            ThreadSafeWriter(std::array<std::string, sizeof...(Ts) + 1> columnNames)
+    public:
+        const static size_t itemDim = sizeof...(Ts) + 1;
+
+        typedef std::array<std::string, itemDim> nameCollectionT;
+
+        typedef std::tuple<U, Ts...> itemT;
+
+        ThreadSafeWriter(nameCollectionT columnNames) : _columnNames(columnNames)
+        {
+
+        }
+
+        ThreadSafeWriter()
+        {
+
+        }
+
+        void SetColumnNames(nameCollectionT columnNames)
+        {
+            _columnNames = columnNames;
+        }
+
+        template <class... Vs> void SetColumnNames(const std::string& name1, const Vs&... names)
+        {
+            static_assert(sizeof...(Vs) == (itemDim - 1), "Column names must be of the same dimension as data.");
+            _columnNames = {name1, names...};
+        }
+
+        virtual ~ThreadSafeWriter() = default;
+
+        void Store(const itemT& item)
+        {
+            std::lock_guard<std::recursive_mutex> lock(_mutex);
+            _data.push_back(item);
+            if (IsFlushRequired())
             {
-
+                Flush();
             }
+        }
 
-            void Store(const std::tuple<U, Ts...>&& item)
+        void Store(const U& first, const Ts&... args)
+        {
+            Store(std::make_tuple(first, args...));
+        }
+
+        virtual bool IsFlushRequired()
+        {
+            return _data.size() == _cacheCapacity;
+        }
+
+        virtual void SetCacheCapacity(size_t capacity)
+        {
+            std::lock_guard<std::recursive_mutex> lock(_mutex);
+            _cacheCapacity = capacity;
+            if (IsFlushRequired())
             {
-                std::lock_guard<std::mutex> lock(_mutex);
-                _data.push_back(item);
+                Flush();
             }
+        }
 
-            void Store(U&& first, Ts&&... args)
-            {
-                Store(std::make_tuple(first, args...));
+        void Flush()
+        {
+            std::cout << "Flushing..." << std::endl;
+            std::lock_guard<std::recursive_mutex> lock(_mutex);
+            for (auto item : _data) {
+                Write(item);
             }
+            _data.clear();
+        }
 
-            void Flush()
-            {
-                std::lock_guard<std::mutex> lock(_mutex);
-                for (auto item : _data) {
-                    Write(item);
-                }
-                _data.clear();
-            }
+    protected:
+        virtual void Write(const std::tuple<U, Ts...>& item) = 0;
 
-        protected:
-            template<std::size_t I = 0, typename... Vs> inline typename std::enable_if<I == sizeof...(Vs), void>::type Write(const std::tuple<Vs...>& t)
-            {
-                std::cout << "\n";
-            }
+        size_t _cacheCapacity = 100;
 
-            template<std::size_t I = 0, typename... Vs> inline typename std::enable_if<I < sizeof...(Vs), void>::type Write(const std::tuple<Vs...>& t)
-            {
-                std::cout << std::get<I>(t) << "\t";
-                Write<I + 1, Vs...>(t);
-            }
+        std::vector<itemT> _data;
 
-        private:
-            std::mutex _mutex;
+        std::recursive_mutex _mutex;
 
-            std::vector<std::tuple<U, Ts...>> _data;
+        nameCollectionT _columnNames;
+    };
+
+    template <class... Ts> class TSVWriter { };
+
+    template <class U, class... Ts> class TSVWriter<U, Ts...> : public ThreadSafeWriter<U, Ts...>
+    {
+    public:
+        ~TSVWriter()
+        {
+            ThreadSafeWriter<U, Ts...>::Flush();
+        }
+
+    protected:
+        virtual void Write(const std::tuple<U, Ts...>& item) override
+        {
+            WriteItem(item);
+        }
+
+        template<std::size_t I = 0, typename... Vs> inline typename std::enable_if<I == sizeof...(Vs), void>::type WriteItem(const std::tuple<Vs...>& t)
+        {
+            std::cout << "\n";
+        }
+
+        template<std::size_t I = 0, typename... Vs> inline typename std::enable_if<I < sizeof...(Vs), void>::type WriteItem(const std::tuple<Vs...>& t)
+        {
+            std::cout << std::get<I>(t) << "\t";
+            WriteItem<I + 1, Vs...>(t);
+        }
     };
 }
